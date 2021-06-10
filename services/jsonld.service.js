8ff3647ace2de22d8ba026e074b67b46b0975ffc
srosset81 <srosset81@gmail.com>
2021-06-10 12:55:31 +0200

    Use local context file

--- services/jsonld.service.js
+++ services/jsonld.service.js
@@ -1,3 +1,16 @@
+const path = require('path');
 const { JsonLdService } = require('@semapps/jsonld');
+const CONFIG = require('../config');
 
-module.exports = JsonLdService;
+module.exports = {
+  mixins: [JsonLdService],
+  settings: {
+    baseUri: CONFIG.HOME_URL,
+    localContextFiles: [
+      {
+        path: '/context.json',
+        file: path.resolve(__dirname, '../public/context.json')
+      }
+    ]
+  }
+};
